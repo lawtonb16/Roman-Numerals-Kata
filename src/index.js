a0993e98c1f0f995ad9e01855b64236dc9fe8e54
Ben Lawton <lawtonb16@gmail.com>
2026-07-11 14:53:24 -0500

    added test for converting I to integer

--- src/index.js
+++ src/index.js
@@ -51,3 +51,7 @@ export function convertToRomanNumeral(number) {
         return convertAbnormalNumberToRomanNumeral(number)
     }
 }
+
+export function convertToInteger(numeral) {
+    
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { convertToRomanNumeral } from "./index.js"
+import { convertToRomanNumeral, convertToInteger } from "./index.js"
 import { test, expect } from "vitest"
 
 test("convert 1 to a roman numeral", () => {
@@ -68,4 +68,8 @@ test("convert 1000 to roman numeral", () => {
 
 test("convert 2022 to roman numeral", () => {
     expect(convertToRomanNumeral(2022)).toBe("MMXXII")
+})
+
+test("convert I to integer", () => {
+    expect(convertToInteger("I")).toBe(1)
 })
